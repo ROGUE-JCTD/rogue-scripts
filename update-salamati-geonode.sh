ce151b574b16c17e41f8ff32049b4a18ddedbeaf
root <root@s2-geoserver-clone.(none)>
2013-06-13 14:28:27 -0700

    changed permissions. minor cleanup

--- update-salamati-geonode.sh
+++ update-salamati-geonode.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-SALAMATI_DIR=temp-salamati
-GEONODE_VIRTUALENV_PATH=/var/lib/geonode-new
-GEONODE_STATIC_PATH=$GEONODE_VIRTUALENV_PATH/geonode/geonode/static
-GEONODE_TEMPLATE_PATH=$GEONODE_VIRTUALENV_PATH/geonode/geonode/templates
+GEONODE_VIRTUALENV_PATH=/var/lib/geonode
+GEONODE_STATIC_PATH=$GEONODE_VIRTUALENV_PATH/rogue_geonode/rogue_geonode/static
+GEONODE_TEMPLATE_PATH=$GEONODE_VIRTUALENV_PATH/rogue_geonode/rogue_geonode/templates
+
+SALAMATI_TEMP_DIR=temp-salamati
+
 pushd .
 cd $GEONODE_VIRTUALENV_PATH
 
 wget -O last-successful-salamati.war http://jenkins.rogue.lmnsolutions.com/job/salamati/lastSuccessfulBuild/artifact/build/salamati.war
-mkdir $SALAMATI_DIR
-unzip last-successful-salamati.war -d $SALAMATI_DIR
+mkdir $SALAMATI_TEMP_DIR
+unzip last-successful-salamati.war -d $SALAMATI_TEMP_DIR
 
 rm -r $GEONODE_STATIC_PATH/sdk/lib
-mv -v $SALAMATI_DIR/lib $GEONODE_STATIC_PATH/sdk/
+mv -v $SALAMATI_TEMP_DIR/lib $GEONODE_STATIC_PATH/sdk/
 rm -r $GEONODE_STATIC_PATH/sdk/theme
-mv -v $SALAMATI_DIR/theme $GEONODE_STATIC_PATH/sdk/
+mv -v $SALAMATI_TEMP_DIR/theme $GEONODE_STATIC_PATH/sdk/
 rm -r $GEONODE_STATIC_PATH/geoexplorer/externals/gxp/src
-mv -v $SALAMATI_DIR/src/gxp/theme $GEONODE_STATIC_PATH/geoexplorer/externals/gxp/src
+mv -v $SALAMATI_TEMP_DIR/src/gxp/theme $GEONODE_STATIC_PATH/geoexplorer/externals/gxp/src
 rm -r $GEONODE_STATIC_PATH/geoexplorer/externals/lightbox
-mv -v $SALAMATI_DIR/src/lightbox $GEONODE_STATIC_PATH/geoexplorer/externals
+mv -v $SALAMATI_TEMP_DIR/src/lightbox $GEONODE_STATIC_PATH/geoexplorer/externals
 rm -r $GEONODE_TEMPLATE_PATH/geonode/sdk_header.html
-mv -v $SALAMATI_DIR/sdk_header.html $GEONODE_TEMPLATE_PATH/geonode/sdk_header.html
+mv -v $SALAMATI_TEMP_DIR/sdk_header.html $GEONODE_TEMPLATE_PATH/geonode/sdk_header.html
 
 pushd .
 cd $GEONODE_VIRTUALENV_PATH
@@ -29,6 +31,5 @@ echo "yes"|python manage.py collectstatic
 chmod 755 -R $GEONODE_STATIC_PATH/../static_root
 popd
 
-rm -r $SALAMATI_DIR
+rm -r $SALAMATI_TEMP_DIR
 popd
-exit
