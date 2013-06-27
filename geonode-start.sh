56e91061114ece5133e51a2a04eda0bcedf5c736
kzusy <kevin.zusy@lmnsolutions.com>
2013-06-27 13:15:10 -0700

    typo in geonode-start and added error checking to it and deloy geonode script

--- geonode-start.sh
+++ geonode-start.sh
@@ -5,8 +5,9 @@ set -e
 # pull in paths
 source /var/lib/rogue-scripts/rogue-variables.sh
 
-pushd .
+is_defined $GEONODE_VIRTUALENV_PATH
+is_defined $GEONODE_ROGUE_REPO_PATH
+
 source $GEONODE_VIRTUALENV_PATH/bin/activate
-cd $GEONODE_ROGUE_REO_PATH
+cd $GEONODE_ROGUE_REPO_PATH
 uwsgi --ini django.ini
-popd
