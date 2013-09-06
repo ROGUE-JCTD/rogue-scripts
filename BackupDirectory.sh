012d6ed1feb420183bff34207602931520b0a0f7
Johnathan Garrett <johnathan.garrett@lmnsolutions.com>
2013-09-06 14:38:56 -0400

    Updated backup script.

--- BackupDirectory.sh
+++ BackupDirectory.sh
@@ -6,7 +6,15 @@ BACKUP_DIR=/path/to/backup/directory
 BACKUP_TARGET=/directory/to/backup
 # Backup prefix - Backups are stored as prefix-date ex. backup-2013-09-05_at_11-41-22
 BACKUP_PREFIX=backup
+# Backup interval in seconds
+BACKUP_INTERVAL=1800
 
-BACKUP_FOLDER=$BACKUP_DIR/$BACKUP_PREFIX-$ROGUE_DATE
-mkdir -p $BACKUP_FOLDER
-cp -r $BACKUP_TARGET/ $BACKUP_FOLDER
+current=`date +%s`
+# NOTE: vv This command does not work on MAC OSX
+last_modified=`date +%s -r $BACKUP_TARGET`
+
+if [ $(($current-$last_modified)) -lt $BACKUP_INTERVAL ]; then
+	BACKUP_FOLDER=$BACKUP_DIR/$BACKUP_PREFIX-$ROGUE_DATE
+	mkdir -p $BACKUP_FOLDER
+	cp -r $BACKUP_TARGET/ $BACKUP_FOLDER
+fi
