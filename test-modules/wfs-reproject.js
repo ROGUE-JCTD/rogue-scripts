b2d9dafbf19a28af28001c4b25317a847c3b05cd
Syrus <syrus.mesdaghi@lmnsolutions.com>
2014-07-01 12:31:26 -0400

    Added script that gets the features of a layers, for features that fall within a bbox, it runs a process function which reprojects them, and then posts them as a wfs update. used for point features.

---
+++ test-modules/wfs-reproject.js
@@ -0,0 +1,228 @@
+"use strict";jQuery.base64=(function($){var _PADCHAR="=",_ALPHA="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",_VERSION="1.0";function _getbyte64(s,i){var idx=_ALPHA.indexOf(s.charAt(i));if(idx===-1){throw"Cannot decode base64"}return idx}function _decode(s){var pads=0,i,b10,imax=s.length,x=[];s=String(s);if(imax===0){return s}if(imax%4!==0){throw"Cannot decode base64"}if(s.charAt(imax-1)===_PADCHAR){pads=1;if(s.charAt(imax-2)===_PADCHAR){pads=2}imax-=4}for(i=0;i<imax;i+=4){b10=(_getbyte64(s,i)<<18)|(_getbyte64(s,i+1)<<12)|(_getbyte64(s,i+2)<<6)|_getbyte64(s,i+3);x.push(String.fromCharCode(b10>>16,(b10>>8)&255,b10&255))}switch(pads){case 1:b10=(_getbyte64(s,i)<<18)|(_getbyte64(s,i+1)<<12)|(_getbyte64(s,i+2)<<6);x.push(String.fromCharCode(b10>>16,(b10>>8)&255));break;case 2:b10=(_getbyte64(s,i)<<18)|(_getbyte64(s,i+1)<<12);x.push(String.fromCharCode(b10>>16));break}return x.join("")}function _getbyte(s,i){var x=s.charCodeAt(i);if(x>255){throw"INVALID_CHARACTER_ERR: DOM Exception 5"}return x}function _encode(s){if(arguments.length!==1){throw"SyntaxError: exactly one argument required"}s=String(s);var i,b10,x=[],imax=s.length-s.length%3;if(s.length===0){return s}for(i=0;i<imax;i+=3){b10=(_getbyte(s,i)<<16)|(_getbyte(s,i+1)<<8)|_getbyte(s,i+2);x.push(_ALPHA.charAt(b10>>18));x.push(_ALPHA.charAt((b10>>12)&63));x.push(_ALPHA.charAt((b10>>6)&63));x.push(_ALPHA.charAt(b10&63))}switch(s.length-imax){case 1:b10=_getbyte(s,i)<<16;x.push(_ALPHA.charAt(b10>>18)+_ALPHA.charAt((b10>>12)&63)+_PADCHAR+_PADCHAR);break;case 2:b10=(_getbyte(s,i)<<16)|(_getbyte(s,i+1)<<8);x.push(_ALPHA.charAt(b10>>18)+_ALPHA.charAt((b10>>12)&63)+_ALPHA.charAt((b10>>6)&63)+_PADCHAR);break}return x.join("")}return{decode:_decode,encode:_encode,VERSION:_VERSION}}(jQuery));
+
+var WfsModule = (function() {
+
+  var config = {
+    // username and password to connect to geoserver when making a Wfs Transaction
+    username: 'syrusm',
+    password: '',
+
+    sourceLayerName: 'geonode:muestreo_techos',
+    destinationLayerName: 'geonode:muestreo_techos',
+    srs: 'EPSG:4326',
+    reprojectSrs: 'EPSG:32616',
+    extent4326: [-92.4527,-1.5818,-88.3081,0.9118]
+  };
+
+  var mapService = angular.element('html').injector().get('mapService');
+  var httpService = angular.element('html').injector().get('$http');
+  var featureList = [];
+  var sourceLayerNameWithoutWorkspace = null;
+  var destinationLayerNameWithoutWorkspace = null;
+
+  function removeWorkspaceFromLayerName(workspaceLayerName) {
+    var colonIndex = workspaceLayerName.indexOf(':');
+    if (colonIndex === -1) {
+      return workspaceLayerName;
+    } else {
+      return workspaceLayerName.substring(colonIndex+1);
+    }
+  }
+
+  function getAllFeatures(callback_success, callback_error) {
+    console.log('---- getAllFeatures');
+    // get as 4326 since you may not get features if they are in the wrong projection because the bbox is validated
+    // by wfs and might be outside of the valid range for the projection
+    var url = '/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&srsName=' + config.srs +
+        '&outputFormat=JSON&typeNames=' + config.sourceLayerName;
+
+    httpService.get(url).then(function(response) {
+      console.log('====>> response: ', response);
+      if (response.data.features.length > 0) {
+        for (var key in response.data.features) {
+          console.log('---- feature: ', response.data.features[key]);
+        }
+
+        featureList = response.data.features;
+        if (callback_success) {
+          callback_success(featureList);
+        }
+      } else {
+        if (callback_error) {
+          callback_error();
+        }
+      }
+    });
+  }
+
+  function postAllFeatures(callback_success, callback_error) {
+    if (config.username && config.password && (typeof config.headerData === 'undefined')) {
+      config.headerData = {
+        'Content-Type': 'text/xml;charset=utf-8',
+        'Authorization': 'Basic ' + $.base64.encode(config.username + ':' + config.password)
+      };
+    }
+
+    var xmlData = getUpdateWfsData();
+    console.log('=================== xmlData: ', xmlData);
+    var url = '/geoserver/wfs/WfsDispatcher';
+    httpService.post(url, xmlData, {headers: config.headerData})
+        .success(function(data, status, headers, config) {
+          console.log('---- data: ', data);
+          console.log('---- status: ', status);
+
+          if (status === 200) {
+
+            // if a feature was inserted, post succeeded
+            if (data.indexOf('<wfs:Status> <wfs:SUCCESS/> </wfs:Status>') !== -1) {
+              console.log('---- success');
+              if (callback_success) {
+                callback_success();
+              }
+            } else if (data.indexOf('ExceptionReport') !== -1) {
+              console.log('====[ TestModule. Wfs Transaction Exception occured: ', data, status, headers, config);
+              var begin = data.indexOf('<ows:ExceptionText>');
+              var end = data.indexOf('</ows:ExceptionText>');
+              var exceptionText = '';
+              if (begin !== -1 && end !== -1) {
+                exceptionText = data.substring(begin + '<ows:ExceptionText>'.length, end);
+              }
+              alert('Wfs-T Exception! See console for response. ExceptionText: ' + exceptionText);
+              if (callback_error) {
+                callback_error();
+              }
+            } else {
+              console.log('====[ TestModule. Unknown Status or Error #1: ', data, status, headers, config);
+              alert('Wfs-T Unknown Status or Error. See console for response.');
+              if (callback_error) {
+                callback_error();
+              }
+            }
+          } else if (status === 401) {
+            console.log('====[ Error: Wfs Transaction, Unauthorized: ', data, status, headers, config);
+            alert('TestModule. Wfs-T, unauthorized. Verify username: ' + config.username +
+                ', password: ' + config.password);
+            if (callback_error) {
+              callback_error();
+            }
+          } else {
+            console.log('====[ TestModule. Unknown Status or Error #2: ', data, status, headers, config);
+            alert('Wfs-T Unknown Status or Error. See console for response.');
+            if (callback_error) {
+              callback_error();
+            }
+          }
+        });
+
+  }
+
+  function processFeatures() {
+    var newFeatures = [];
+    for (var key in featureList) {
+      var feature = featureList[key];
+      console.log('---- feature: ', feature, ', post processed: ', feature);
+      if (ol.extent.containsCoordinate(config.extent4326, feature.geometry.coordinates)) {
+        // fails on getCode! have to use apply transform instead
+        //var transform = ol.proj.getTransform(config.srs, config.reprojectSrs);
+        var transform = ol.proj.getTransformFromProjections(ol.proj.get("EPSG:32616"), ol.proj.get("EPSG:4326"));
+        var point = new ol.geom.Point(feature.geometry.coordinates);
+
+        point.applyTransform(ol.proj.getTransform("EPSG:4326","EPSG:32616"))
+        feature.geometry.coordinates = point.flatCoordinates;
+        newFeatures.push(feature);
+      }
+    }
+
+    console.log('in count: ', featureList.length, ', out count: ', newFeatures.length);
+    console.log('new features: ', newFeatures);
+
+    featureList = newFeatures;
+  }
+
+  function getWfsFeaturesXml() {
+    var xml = '';
+    var geometryName = null;
+    console.log('==== featureList: ', featureList);
+    for (var index in featureList) {
+      var feature = featureList[index];
+      console.log('---- feature: ', feature);
+      xml += '<wfs:Update typeName="' + config.destinationLayerName + '">' +
+      '<wfs:Property>' +
+      '<wfs:Name>' + feature.geometry_name + '</wfs:Name>' +
+      '<wfs:Value>' +
+          '<gml:Point xmlns:gml="http://www.opengis.net/gml" srsName="' + config.srs + '">' +
+          '<gml:coordinates decimal="." cs="," ts=" ">' + feature.geometry.coordinates[0] + ',' + feature.geometry.coordinates[1] + '</gml:coordinates>' +
+          '</gml:Point>' +
+      '</wfs:Value>' +
+      '</wfs:Property>' +
+      '<ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">' +
+        '<ogc:FeatureId fid="' + feature.id + '"/>' +
+      '</ogc:Filter>' +
+      '</wfs:Update>';
+    }
+    return xml;
+  }
+
+  function getUpdateWfsData() {
+    var xml = '' +
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<wfs:Transaction xmlns:wfs="http://www.opengis.net/wfs"' +
+        ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+        'service="WFS" version="1.0.0" handle="reprojecting features from 4326 to 32616 using wfs-reproject script">' +
+        getWfsFeaturesXml() +
+        '</wfs:Transaction>';
+
+    return xml;
+  }
+
+  var p = {};
+
+  p.start = function() {
+    var successCallback = function(){
+      console.log('----> Mapping validated');
+      getAllFeatures(function(features) {
+        console.log('----[ featurs.length: ', features.length);
+        processFeatures();
+        postAllFeatures();
+      }, function() {
+        console.log('====[ Error: failed to get current features of the layer. Not starting test!');
+      });
+    };
+
+    var errorCallback = function(){
+      console.log('====[ ERROR: validation error');
+    };
+
+    sourceLayerNameWithoutWorkspace = removeWorkspaceFromLayerName(config.sourceLayerName);
+    destinationLayerNameWithoutWorkspace = removeWorkspaceFromLayerName(config.destinationLayerName);
+    successCallback();
+  };
+
+  p.getConfig = function() {
+    return config;
+  };
+
+  return p;
+}());
+
+WfsModule.start();
+
+
+/*
+ <?xml version="1.0" encoding="UTF-8"?>
+ <wfs:Transaction xmlns:wfs="http://www.opengis.net/wfs" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" service="WFS" version="1.0.0" handle="Modified 1 feature in 'vulnerabilidad_estructural' via MapLoom." xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/wfs.xsd">
+ <wfs:Update xmlns:feature="http://www.geonode.org/" handle="Modified 1 feature in 'vulnerabilidad_estructural' via MapLoom." typeName="geonode:vulnerabilidad_estructural">
+ <wfs:Property>
+ <wfs:Name>geom</wfs:Name>
+ <wfs:Value>
+ <gml:Point xmlns:gml="http://www.opengis.net/gml" srsName="EPSG:3857">
+ <gml:coordinates decimal="." cs="," ts=" ">-9609979.392133355,1344290.8197953443</gml:coordinates>
+ </gml:Point>
+ </wfs:Value>
+ </wfs:Property>
+ <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+ <ogc:FeatureId fid="fid--4593c659_146f2590dd9_-7ffe" />
+ </ogc:Filter>
+ </wfs:Update>
+ </wfs:Transaction>
+ */
